7b76aa09802fc534777b2d3392f41c05630587d3
NAMAN TYAGI <namantyagi33@gmail.com>
2023-05-28 13:03:05 +0530

    Lecture 13

---
+++ Lecture13/main.js
@@ -0,0 +1,228 @@
+
+// print();
+// // Logical operators
+
+// // console.log( 0 || null || "");
+
+// // console.log( "1" && "2" && true);
+
+// // console.log(!true)
+
+
+
+// //  Functions
+
+// function print(){
+
+//     console.log("hello world")
+
+// }
+
+// print();
+// print();
+
+// func()
+
+// let sum= function(){
+
+
+//     console.log("this is function expression");
+
+//     return "deepanshi";
+// }
+
+// console.log(sum())
+
+
+
+
+// Arrow function 
+
+// let square =  (a , b) =>  {
+
+//     return a * a;
+// }  
+
+// console.log(square(3));
+
+
+
+// // Strings  in JS
+
+// let myString = "this is a string";
+
+// console.log(myString[3]);
+
+// console.log(myString.length);
+
+// console.log(myString.at(-3));
+
+
+// 
+
+// for(let i of myString ){
+
+//     console.log(i)
+// }
+
+
+
+
+// 
+
+// let str = "this is a string";
+
+// // console.log(str.indexOf("is" , -2))
+
+// //
+
+// console.log(str.slice(4,7));
+
+
+
+
+//   Numbers in JS
+
+
+
+// let num = 20;
+// console.log(num);
+
+// num = 1e-4;
+
+// console.log(num)
+
+// num = 1234567788433n;
+
+// console.log(num , typeof num)
+
+
+// let num = 255;
+
+// console.log(num.toString(16));
+
+
+// console.log(parseFloat("3.14"))
+
+// console.log(Math.floor(Math.random()*10));
+
+// console.log(Math.pow( 3,4));;
+// console.log(Math.sqrt(81))
+
+
+// Arrays in JS
+
+
+// const arr = [];
+
+// const arr = new Array(10);
+
+
+// const arr = [1, "string", true, {}, function(){
+//     console.log("hello world")
+
+//     return 1;
+// }];
+
+// console.log(arr.length);
+
+// console.log(arr.pop());
+
+// console.log(arr.push("apples"));
+
+// console.log(arr.unshift("grapes"));
+
+// console.log(arr.shift())
+
+// console.log(arr);
+
+// console.log(arr.slice(0,3));
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+
+// console.log(arr.splice(3,0, "apple"));
+
+// console.log(arr);
+
+
+// console.log(arr.concat([1,2], [3,4]));
+
+// console.log(arr);
+
+
+
+// filter method 
+
+// let even  = arr.filter((n)=> n % 2 === 0 );
+
+// console.log(even);
+
+// // find
+
+// let element = arr.find((ele, ind, arr)=> n % 2 === 0);
+
+// console.log(element)
+
+
+//  console.log( 0 !== "0")
+
+
+
+//  user interaction
+
+// alert(" hello!");
+
+// let age = prompt(" what is your age?", 25);
+
+// console.log(age);
+
+// let arr = [1,2,3,4,5,6,7,8, 9,10];
+
+
+// const filteredArr = arr.filter((ele, index,arr)=>{
+
+//    if( ele % 2 === 0){
+
+//         console.log(` your element ${ele} is present at index ${index} in array ${arr}`);   
+//     }  
+
+//     return (ele % 2=== 0)
+// });
+
+// console.log(filteredArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
